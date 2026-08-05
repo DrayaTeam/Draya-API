684bb34a77223738cfe5d6bf98e22b4f3139ab4b
OmarMohammed <omarbenmohamed2003@gmail.com>
2026-08-05 18:44:27 +0300

    refactor: letting other teams access api

--- src/Draya.Api/Program.cs
+++ src/Draya.Api/Program.cs
@@ -33,11 +33,11 @@ var app = builder.Build();
 
 app.UseMiddleware<GlobalExceptionMiddleware>();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
